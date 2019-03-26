4550f972d434c91c74ca68725bf267a9365c3dc4
Raul Quintero <raulquintero13@gmail.com>
2019-03-26 15:31:08 -0700

    test

--- js/app.js
+++ js/app.js
@@ -4,3 +4,22 @@ if (navigator.serviceWorker){
     console.log('supported');
     navigator.serviceWorker.register('/sw.js');
 }
+
+
+var wifi = document.querySelector('#wifi');
+
+// document.addEventListener("DOMContentLoaded", function () {
+// setInterval(() => {
+//     fetch('http://localhost:8080/img/onlineico.png')
+//         .then( ()=>{
+//             console.log('online');
+//             wifi.src = 'http://localhost:8080/img/onlineico.png'
+
+//         })
+//         .catch(()=>{
+//             console.log('offline');
+//             wifi.src = '/img/offlineico.png'
+//         });
+// }, 3000);
+
+// });
--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
 
-const CACHE_STATIC_NAME     = "static-v1";
-const CACHE_INMUTABLE_NAME  = "inmutable-v1";
-const CACHE_DYNAMIC_NAME    = "dynamic-v1";
+importScripts('/js/sw-utils.js');
 
+const CACHE_STATIC_NAME = "static-v1";
+const CACHE_INMUTABLE_NAME = "inmutable-v1";
+const CACHE_DYNAMIC_NAME = "dynamic-v1";
+const OFFLINE_ICON = "/img/offlineIco.png";
 
 
 self.addEventListener('install', e => {
-    
-    const cacheProm = caches.open( CACHE_STATIC_NAME )
-        .then( cache => {
+
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
+        .then(cache => {
 
             return cache.addAll([
                 '/',
@@ -16,16 +18,93 @@ self.addEventListener('install', e => {
                 '/style.css',
                 '/img/main.jpg',
                 '/js/app.js',
-            ])
+                '/img/offlineico.png',
+                '/img/onlineico.png',
+                '/aboutus.html'
+            ]);
         });
-    
-        const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
-            .then( cache => {
-                return  cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')
-            });
-    
-    e.respondWith( Promise.all([cacheProm, cacheInmutable]) );
 
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+                'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+            ]);
+        });
+
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 
 
 })
+
+
+self.addEventListener('activate', e => {
+
+
+    const respuesta = caches.keys().then(keys => {
+
+        keys.forEach(key => {
+
+            // static-v4
+            if (key !== CACHE_STATIC_NAME && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+
+
+    e.waitUntil(respuesta);
+
+});
+
+self.addEventListener('fetch', e => {
+
+
+   
+    const respuesta = new Promise( (resolve, reject) =>{
+
+        let rechazada = false;
+
+        const falloUnaVez = () => {
+
+            if ( rechazada ) {
+                
+                if ( /\.(png|jpg)$/i.test( e.request.url ) ) {
+
+                    resolve( caches.match('/img/no-img.jpg')  );
+
+                } else { 
+                    reject('No se encontro respuesta');
+                }
+
+
+            } else {
+                rechazada = true;
+            }
+
+
+        };
+
+
+
+        fetch( e.request ).then( res => {
+            res.ok ? resolve(res) : falloUnaVez();
+        }).catch( falloUnaVez );
+
+
+        caches.match( e.request ).then( res => {
+            res ? resolve( res ) : falloUnaVez();
+        }).catch( falloUnaVez );
+
+
+    });
+
+
+    e.respondWith( respuesta );
+
+});
